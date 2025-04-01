34cd3541e40ef2df37942fca24855853e3acc915
s25819 <s25819@pjwstk.edu.pl>
2025-04-01 23:56:11 +0200

    first main screen design

--- app/src/main/java/pl/edu/pjatk/s25819/przeterminarz/MainActivity.kt
+++ app/src/main/java/pl/edu/pjatk/s25819/przeterminarz/MainActivity.kt
@@ -5,16 +5,29 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+import pl.edu.pjatk.s25819.przeterminarz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater).apply {
+            setContentView(root)
+        }
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        Snackbar.make(binding.main, "Hello!", Snackbar.LENGTH_SHORT).show()
+
     }
 }
