5bb740fa48c41015dbcdcfae5cc8492b4c099b4c
s25819 <s25819@pjwstk.edu.pl>
2025-04-22 17:38:28 +0200

    added viewmodel dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,10 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    // ViewModel
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    // LiveData
+    implementation(libs.androidx.lifecycle.livedata.ktx)
     implementation(libs.androidx.navigation.fragment)
     implementation(libs.androidx.material3.android)
     testImplementation(libs.junit)
